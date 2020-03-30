a2c08acdf280aab1d132b1e1ecdd768827733571
James Gallagher <jgallagher@opendap.org>
2020-03-30 14:54:36 -0600

    Updated comments in get_bandgap()

--- src/Soil_moisture_2.0.cc
+++ src/Soil_moisture_2.0.cc
@@ -153,17 +153,18 @@ uint16_t get_bandgap()   // Returns actual value of Vcc (x 100)
 #if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
     // For mega boards
     const long InternalReferenceVoltage = 1115L;// Adjust this value to your boards specific internal BG voltage x1000
-    // REFS1 REFS0          --> 0 1, AVcc internal ref. -Selects AVcc reference
+    // REFS1 REFS0               --> 0 1, AVcc internal ref.  -Selects AVcc reference
     // MUX4 MUX3 MUX2 MUX1 MUX0  --> 11110 1.1V (VBG)         -Selects channel 30, bandgap voltage, to measure
-    ADMUX = (0 << REFS1) | (1 << REFS0) | (0 << ADLAR) | (0 << MUX5) | (1 << MUX4) | (1 << MUX3) | (1 << MUX2) | (1 << MUX1) | (0 << MUX0);
-
+    ADMUX = (0 << REFS1) | (1 << REFS0) | (0 << ADLAR)
+            | (0 << MUX5) | (1 << MUX4) | (1 << MUX3) | (1 << MUX2) | (1 << MUX1) | (0 << MUX0);
 #else
     // For 168/328 boards
     const long InternalReferenceVoltage = AREF_VOLTAGE_X1000; // Adjust this value to your boards specific internal BG voltage x1000
     // REFS1 REFS0          --> 0 1, AVcc internal ref. -Selects AVcc external reference
     // MUX3 MUX2 MUX1 MUX0  --> 1110 1.1V (VBG)         -Selects channel 14, bandgap voltage, to measure
-    ADMUX = (0 << REFS1) | (1 << REFS0) | (0 << ADLAR) | (1 << MUX3)
-            | (1 << MUX2) | (1 << MUX1) | (0 << MUX0);
+    // ADLAR                --> 0                       -Selects ADCL bit0-7 and ADCH bit8-9
+    ADMUX = (0 << REFS1) | (1 << REFS0) | (0 << ADLAR)
+            | (1 << MUX3) | (1 << MUX2) | (1 << MUX1) | (0 << MUX0);
 #endif
 
     //delay(50);  // Let mux settle a little to get a more stable A/D conversion
